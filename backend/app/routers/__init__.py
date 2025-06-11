c2eba68278f9b40f67e94cc5202b5d0067dee9eb
cucuruchi-project <164995024+2gyuho@users.noreply.github.com>
2025-06-12 01:26:33 +0900

    여행 라우터를 __init__.py에 추가하여 __all__ 목록 업데이트

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -2,5 +2,6 @@ from .embassy import router as embassy_router
 from .news import router as news_router
 from .auth import router as auth_router
 from .geolocation import router as geolocation_router
+from .travel import router as travel_router
 
-__all__ = ["embassy_router", "news_router", "auth_router", "geolocation_router"]
+__all__ = ["embassy_router", "news_router", "auth_router", "geolocation_router", "travel_router"]
